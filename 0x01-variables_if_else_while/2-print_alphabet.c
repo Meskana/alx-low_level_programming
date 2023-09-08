fe8d9d89a23686ed9189dc8c5dee1497dd105594
meskana <marcelinusilonuba21@gmail.com>
2023-09-08 13:52:07 -0700

    olny

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Execution starts here
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
