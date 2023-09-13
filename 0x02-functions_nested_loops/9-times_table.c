7d36c252377b95abdd7cf1033459320be15c527a
meskana <marcelinusilonuba21@gmail.com>
2023-09-12 17:01:27 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * time_table - print the 9 time table, starting with 0
+ */
+void time_table(void)
+{
+	int t, m, b;
+
+	for (t = 0; t < 10; t++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			b = m * t;
+
+			if (m == 0)
+			{
+				_putchar(b + '0');
+			}
+			if (b < 10 && m != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(b + '0');
+			}
+			else if (b >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((b / 10) + '0');
+				_putchar((b % 10) + '0');
+			}
+		}
+		 _putchar('\n');
+	}
+}
