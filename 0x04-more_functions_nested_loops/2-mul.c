e339f6c30aae2d791997ac40e0b4bd4afb242208
meskana <marcelinusilonuba21@gmail.com>
2023-09-14 12:11:54 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two numbers
+ * @a: parameter1
+ * @b: parameter2
+ * Return: Returns 0
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	num = 0;
+	while (num <= 9)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
+	num++;
+	}
+	_putchar('\n');
+}
