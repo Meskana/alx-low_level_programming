c142b95253cbf24139659466c905d3c47aa107f3
meskana <marcelinusilonuba21@gmail.com>
2023-09-18 07:09:17 -0700

    alx-low_level_programming

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,13 +7,14 @@
 void rev_string(char *s)
 {
 	int i;
-	char j;
 	int count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 		count++;
 	for (i = 0; i < count / 2; i++)
 	{
+		char j;
+
 		j = s[i];
 		s[i] = s[count - 1 - i];
 		s[count - 1 - i] = j;
