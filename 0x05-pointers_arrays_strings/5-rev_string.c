4fa230475b79c28babef027d442f5a5e046f7e2c
meskana <marcelinusilonuba21@gmail.com>
2023-09-18 06:46:01 -0700

    alx-low_level_programming

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_sting - reverses astring
+ * @s: string to be reversed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+	char j;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = 0; i < count / 2; i++)
+	{
+		j = s[i];
+		s[i] = s[count - 1 - i];
+		s[count - 1 - i] = j;
+	}
+}
