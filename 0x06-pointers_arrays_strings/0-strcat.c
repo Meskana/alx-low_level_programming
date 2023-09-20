a410da01cc055e2180bda2206bab32ef3064577a
meskana <marcelinusilonuba21@gmail.com>
2023-09-20 13:52:20 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatenates two strings dest and src
+ * @dest: to the  destination
+ * @src: from the source
+ * Return: return character
+ */
+char *_strcat(char *dest, char *src)
+{
+	int l1 = 0, l2 = 0,i;
+	
+	l1 = strlen(dest);
+	l2 = strlen(src);
+	for (i = 0; i <= l2; i++)
+	{
+		dest[l1 = i]  = src[i];
+	}
+	return (dest);
+}
