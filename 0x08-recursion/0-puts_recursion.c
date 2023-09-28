020cf62c907485734b8af630b311636eb88bc142
meskana <marcelinusilonuba21@gmail.com>
2023-09-28 16:25:29 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,18 @@
+#include "main.h"
+
 /**
- * _puts_recursion - print string
- * @s: string to print
+ * _puts_recursion - prints a string
+ * @s: string to be printed
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
-	else
-	{
-		_putchar("\n");
-	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
