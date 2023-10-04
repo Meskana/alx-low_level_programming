33bdd83d23443c3b5401e13ae722c02adad3cad1
meskana <marcelinusilonuba21@gmail.com>
2023-10-04 10:51:19 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+nclude "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: character to store
+ * Return: pointer to character
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
