d04163e9cc3cdb34901738266b8a8d75439fc2ab
meskana <marcelinusilonuba21@gmail.com>
2023-10-04 16:21:48 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers
@@ -30,3 +30,6 @@ int **alloc_grid(int width, int height)
 			{
 				free(result[j]);
 			}
+		}
+	}
+}
