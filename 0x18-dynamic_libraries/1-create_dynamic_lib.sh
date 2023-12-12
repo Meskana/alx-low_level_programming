967f47174eb1e118220cdac9a91c0dc13fe8576f
meskana <marcelinusilonuba21@gmail.com>
2023-12-11 17:17:00 -0800

    shell script

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -Pendantic -c -fPIC -c *.c
-gcc -shared -o libdynamic.so *.o
+gcc -Wall -Wextra -Werror -Pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
